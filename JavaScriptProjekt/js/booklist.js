f5d4ba6c027ce4cd4c18ef31b33793c411b5b4bf
David489 <pav22044@spengergasse.at>
2024-06-01 19:39:41 +0200

    Book List V0.3

--- JavaScriptProjekt/js/booklist.js
+++ JavaScriptProjekt/js/booklist.js
@@ -1,5 +1,6 @@
 // 1. Application State
-class Book {
+class Book 
+{
     #title;
     #author;
     #genre;
@@ -15,9 +16,29 @@ class Book {
         this.#price = price;
     }
 
-    getBooks()
+    get Title()
     {
-        return state.books;
+        return this.#title;
+    }
+
+    get Author()
+    {
+        return this.#author;
+    }
+
+    get Genre()
+    {
+        return this.#genre;
+    }
+
+    get IsHardcover()
+    {
+        return this.#hardcover;
+    }
+
+    get Price()
+    {
+        return this.#price;
     }
 }
 
@@ -35,8 +56,17 @@ function addNewBookToState(title, author, genre, hardcover, price)
     state.books.push(new Book(title, author, genre, hardcover, price));
 }
 
+function delBookFromState(book)
+{
+    let idx = state.books.indexOf(book);
+    if(idx !== -1)
+    {
+        state.books.splice(idx, 1);
+    }
+}
+
 //3. DOM Node References
-const booklist = document.getElementById('book-list');
+const itemlist = document.getElementById('book-list');
 const bookAdd = document.getElementById('book-add');
 const titleInput = document.getElementById('book-title');
 const authorInput = document.getElementById('author');
@@ -47,18 +77,61 @@ const priceInput = document.getElementById('price');
 //4. DOM Node Creation Functions
 function createBookElement(book)
 {
+    const bookItem = document.createElement('tr');
 
+    const bookTitle = document.createElement('td');
+    const bookAuthor = document.createElement('td');
+    const bookGenre = document.createElement('td');
+    const bookHardcover = document.createElement('td');
+    const bookPrice = document.createElement('td');
+
+    bookTitle.textContent = book.Title;
+    bookAuthor.textContent = book.Author;
+    bookGenre.textContent = book.Genre;
+    bookHardcover.textContent = book.IsHardcover ? "True" : "False";
+    bookPrice.textContent = book.Price;
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'Delete';
+    delBtn.addEventListener('click', (event) => delBookBtn(book));
+
+    bookItem.appendChild(bookTitle);
+    bookItem.appendChild(bookAuthor);
+    bookItem.appendChild(bookGenre);
+    bookItem.appendChild(bookHardcover);
+    bookItem.appendChild(bookPrice);
+    
+    bookItem.appendChild(delBtn);
+
+    return bookItem;
 }
 
 //5. Render Function
 function render()
 {
-    booklist.innerHTML = '';
-    const books = getBooks();
-    books.forEach(book => {
-        booklist.appendChild(createBookElement(book));
+    itemlist.innerHTML = '';
+    const booklist = state.books;
+    booklist.forEach(book => {
+        itemlist.appendChild(createBookElement(book));
     });
 }
+
+//6. Event Handlers
+function addBookBtn()
+{
+    event.preventDefault();
+    addNewBookToState(titleInput.value, authorInput.value, genreInput.value, hardcover.value, priceInput.value);
+    render();
+}
+
+function delBookBtn(book)
+{
+    delBookFromState(book);
+    render();
+}
+
+//7. Initial Bindings
+bookAdd.addEventListener('click', addBookBtn);
 
 //8. Initial Render
 render();
